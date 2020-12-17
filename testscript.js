691c3dc304f059f751110312730c77240244259a
sonalbansal465 <76146582+sonalbansal465@users.noreply.github.com>
2020-12-17 13:37:29 +0530

    Update and rename testscript.html to testscript.js

--- testscript.js
+++ testscript.js
@@ -1,10 +1,8 @@
 
-<script>
 var url = window.location.href;
 console.log(url,'urlll');
 var checkoutId = url.split("/")[5];
 var redirect_url = "https://www.w3schools.com?checkoutid="+ checkoutId;
 console.log(redirect_url,'redirect_url???');
 window.location = redirect_url
-</script>
 
