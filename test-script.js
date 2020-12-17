00a6419a4e9582ac3c56ffd591866d316a6551d3
sonalbansal465 <76146582+sonalbansal465@users.noreply.github.com>
2020-12-17 12:27:28 +0530

    Update test-script.js

--- test-script.js
+++ test-script.js
@@ -1,5 +1,6 @@
 
 var url = window.location.href;
+console.log(url,'urlll');
 var checkoutId = url.split("/")[5];
 var redirect_url = "https://www.w3schools.com?checkoutid="+ checkoutId;
 console.log(redirect_url,'redirect_url???');
